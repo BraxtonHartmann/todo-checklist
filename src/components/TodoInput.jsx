a24d877ce3b8aa33e3a0c1e9a95107b1b8d26c09
Braxton Hartmann <braxtonhartmann@mail.weber.edu>
2026-09-21 09:42:36 -0600

    initial commit

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import Button from './Button';
+import './TodoInput.css';
+
+const TodoInput = props => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
+
+  const todoInputChangeHandler = event => {
+    if(event.target.value.trim().length > 0){
+      setIsValid(true);
+    }
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = event => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddTodo(enteredValue);
+    setEnteredValue('');
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={`form-control ${isValid ? '' : 'invalid'}`}>
+        <label /*style={{color: isValid ? 'black':'red'}}*/>Checklist Task</label>
+        <input
+          // style = {{
+          //   borderColor: isValid ? '#ccc' : 'red',
+          //   backgroundColor: isValid ? 'transparent':'salmon'
+          // }}
+          type="text"
+          value={enteredValue}
+          onChange={todoInputChangeHandler}
+        />
+      </div>
+      <Button type="submit">Add Task</Button>
+    </form>
+  );
+};
+
+export default TodoInput;
